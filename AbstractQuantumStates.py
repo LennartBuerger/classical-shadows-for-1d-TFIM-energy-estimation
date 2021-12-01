c829838870bddb760d0b312ae1d3b20ee78ff1e8
LennartBuerger <95316583+LennartBuerger@users.noreply.github.com>
2021-12-01 16:58:37 +0100

    Add files via upload

---
+++ AbstractQuantumStates.py
@@ -0,0 +1,43 @@
+from abc import ABC, abstractmethod
+import torch as pt
+
+
+class AbstractQuantumStates(ABC):
+    # we pass the number of qubits N
+    def __init__(self, N):
+        self.N = N
+
+
+    # measuring amplitude with respect to some basis vector
+    @abstractmethod
+    def amplitude(self, basis_idx: pt.Tensor) -> float:
+        pass
+
+    # probability of measuring our quantum state in a certain basis vector state
+    @abstractmethod
+    def prob(self, basis_idx: pt.Tensor) -> float:
+        pass
+
+    @abstractmethod
+    def norm(self) -> float:
+        pass
+
+    # measures state in computational basis, B is number of samples we take
+    @abstractmethod
+    def measure(self, batch_size: int) -> dict:
+        pass
+
+    # takes a pauli string and rotates to the basis given by this string, returns a new instance of our quantum state
+    @abstractmethod
+    def rotate_pauli(self, pauli_string: dict):
+        pass
+
+    # here we rotate first and then do a measurement in the computational basis
+    @abstractmethod
+    def measure_pauli(self, pauli_string: dict) -> dict:
+        pass
+
+    # apply a string of single qubit clifford gates
+    @abstractmethod
+    def apply_clifford(self, clifford_string: dict):
+        pass
