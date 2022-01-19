98b7cd5f80bb87b22a0aa93f308644f3c957a5da
Lennart <Lennart.Buerger@gmail.com>
2022-01-19 18:57:44 +0100

    added the two classes data_acquisition_shadow and prediction_shadow from the Preskill shadow paper, added the function shadow_energy in 1d_tfim_hamiltonian: this function accesses the two classes from the shadow paper to predict the energy of an arbitrary state, I added the notebook shadow_performance_analysis where the ground state energy prediction via shadows is compared with the exact solution, I rewrote the function rotate_pauli in brute_force_quantum_state such that it scales up to twenty qubits (10min for energy prediction with one measurement per observable for all ratios), I used sparce matrices for that

--- src/tfim_hamiltonian_open_fermion.py
+++ src/tfim_hamiltonian_open_fermion.py
@@ -7,12 +7,15 @@ import scipy.sparse.linalg
 import numpy as np
 import matplotlib.pyplot as plt
 import constants
+from data_acquisition_shadow import derandomized_classical_shadow
+from prediction_shadow import estimate_exp
+from bf_quantum_state import BFQuantumState
 
 from abstract_quantum_state import AbstractQuantumState
 
 
 class TfimHamiltonianOpenFermion(abstract_hamiltonian.AbstractHamiltonian, ABC):
-    ENERGY_METHODS = ('BF',)
+    ENERGY_METHODS = ('BF', 'BF_shadow')
     BOUNDARY_CONDITIONS = ('open', 'periodic',)
 
     # we pass the number of qubits N
@@ -31,6 +34,72 @@ class TfimHamiltonianOpenFermion(abstract_hamiltonian.AbstractHamiltonian, ABC):
 
     def energy_bf(self, psi: pt.tensor):
         return pt.conj(psi) @ pt.tensor(self.to_matrix().dot(psi), dtype=self.dtype)
+
+    def energy_shadow(self, psi: pt.tensor, num_of_measurements_per_observable: int):
+        global observables
+        if psi is None:
+            psi = self.ground_state_wavevector()
+        # first create observables.txt with all the terms in the Hamiltonian inside
+        assert self.boundary_cond in TfimHamiltonianOpenFermion.BOUNDARY_CONDITIONS
+        if self.boundary_cond == 'periodic':
+            observables = []
+            for i in range(0, self.qubit_num):
+                x_arr = [['X', i]]
+                if i <= self.qubit_num - 2:
+                    z_arr = [['Z', i], ['Z', i + 1]]
+                elif i <= self.qubit_num - 1:
+                    z_arr = [['Z', i], ['Z', 0]]
+                else:
+                    z_arr = None
+                observables.append(x_arr)
+                observables.append(z_arr)
+        # feed observables to derandomized classical shadow
+        measurement_procedure = derandomized_classical_shadow(observables,
+                                                              num_of_measurements_per_observable, self.qubit_num)
+        # convert the array measurement_procedure to array of dicts to have the right format for the measurement
+        measurement_procedure_dict = []
+        for i in range(0, len(measurement_procedure)):
+            pauli_dict = {}
+            for j in range(0, len(measurement_procedure[i])):
+                pauli_dict[j] = measurement_procedure[i][j]
+            measurement_procedure_dict.append(pauli_dict)
+        # now we apply the measurements to our state psi
+        measurement_index = []
+        for i in range(0, len(measurement_procedure_dict)):
+            measurement_index.append(BFQuantumState(self.qubit_num,
+                                                    psi).measure_pauli(measurement_procedure_dict[i], 1)[0])
+        # the measured index which is just a number has to be converted to the appropriate shape for prediction shadow
+        # which is e.g [[[X, 1],[Z, -1]],[[X, 1], [Z, 1]] etc. (this would correspond to the two measured states
+        # |01> and |00>
+        measurement_array_full = []
+        for i in range(0, len(measurement_index)):
+            measurement_dirac_rep = str(bin(measurement_index[i]))
+            measurement_array = np.array(list(measurement_dirac_rep))
+            measurement_array = np.delete(measurement_array, [0, 1])
+            if np.size(measurement_array) != self.qubit_num:
+                measurement_array = np.append(np.zeros(self.qubit_num - np.size(measurement_array)),
+                                              measurement_array)
+            measurement_array_full.append(measurement_array)
+        measurement = []
+        for i in range(0, len(measurement_procedure)):
+            measurement_part = []
+            for j in range(0, self.qubit_num):
+                if measurement_array_full[i][j] == '0':
+                    measured_value = 1
+                else:
+                    measured_value = -1
+                measurement_part.append([measurement_procedure[i][j], measured_value])
+            measurement.append(measurement_part)
+        # now we have our measurement outcome stored in measurement in the correct format
+        energy = 0
+        for i in range(0, len(observables)):
+            sum_product, cnt_match = estimate_exp(measurement, observables[i])
+            expectation_val = sum_product / cnt_match
+            if i % 2 == 0:
+                energy = energy + self.h * expectation_val
+            else:
+                energy = energy + self.J * expectation_val
+        return energy
 
     def diagonalize(self, nr_eigs: int, return_eig_vecs: bool) -> (pt.float, pt.tensor):
         if return_eig_vecs:
@@ -78,22 +147,25 @@ class TfimHamiltonianOpenFermion(abstract_hamiltonian.AbstractHamiltonian, ABC):
 
     # for chain of finite size
     def theo_energygap_finite_size(self) -> pt.float:
-        if self.boundary_cond == 'open':
-            energy_gap = 2 * np.abs(self.J) * np.sqrt((np.cos(np.pi / self.qubit_num) - self.h / self.J) ** 2
-                                                      + np.sin(np.pi / self.qubit_num) ** 2)
         if self.boundary_cond == 'periodic':
-            if self.h <= 1:
-                n_vals = pt.linspace(1, self.qubit_num / 2, int(self.qubit_num / 2))
-                k_abc = (2 * n_vals - pt.ones(int(self.qubit_num / 2))) * pt.pi / self.qubit_num
-                energy_eigenvalues = 2 * self.J * pt.sqrt((pt.cos(k_abc) - self.h / self.J) ** 2 + pt.sin(k_abc) ** 2)
-                energy_gap = 2 * pt.min(energy_eigenvalues)
+            # first excited state in the ABC sector
+            n_abc = pt.linspace(1, int(self.qubit_num / 2), int(self.qubit_num / 2))
+            k_abc = (2 * n_abc - 1) * pt.pi / self.qubit_num
+            energy_gap_abc = 2 * pt.min(
+                2 * self.J * pt.sqrt((pt.cos(k_abc) - self.h / self.J) ** 2 + pt.sin(k_abc) ** 2))
+
+            # ground state energy in the PBC sector
+            n_pbc = pt.linspace(1, int(self.qubit_num / 2) - 1, int(self.qubit_num / 2) - 1)
+            k_pbc = (2 * n_pbc) * pt.pi / self.qubit_num
+            ground_energy_pbc = -2 * self.J - pt.sum(
+                2 * self.J * pt.sqrt((pt.cos(k_pbc) - self.h / self.J) ** 2 + pt.sin(k_pbc) ** 2))
+            energy_gap_pbc = pt.abs(self.ground_state_energy_theo()) - pt.abs(ground_energy_pbc)
+
+            if self.h / self.J > 1:
+                return pt.min(pt.tensor([energy_gap_abc, energy_gap_pbc]))
             else:
-                energy_gap = 2 * np.abs(self.J) * np.sqrt((np.cos(1 * np.pi / self.qubit_num) - self.h / self.J) ** 2
-                                                          + np.sin(1 * np.pi / self.qubit_num) ** 2)
-
-        return energy_gap
-
-    # calculate theoretical prediction of the energy gap
+                # there are only two energygaps in pt.max, thus using pt.max the second lowest energygap is choosen
+                return pt.max(pt.tensor([energy_gap_abc, energy_gap_pbc]))
 
     def ground_state_wavevector(self) -> pt.tensor:
         eigenvalues, eigenvector = self.diagonalize(1, True)
@@ -132,7 +204,7 @@ def main():
     # print(TfimHamiltonianOpenFermion(qubit_num, 2, 1, 'periodic').ground_state_energy())
     # print(TfimHamiltonianOpenFermion(qubit_num, 2, 1, 'periodic').ground_state_energy_theo())
 
-    TfimHamiltonianOpenFermion(24, 1, 1, 'periodic').diagonalize(2, True)
+    print(TfimHamiltonianOpenFermion(12, 0.5, 1, 'periodic').energy_shadow(None, 5))
 
 
 if __name__ == '__main__':
