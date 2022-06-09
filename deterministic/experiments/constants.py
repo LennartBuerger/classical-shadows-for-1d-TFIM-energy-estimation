171cbb8c291951aa5c46590a5e26560893f14364
Lennart <Lennart.Buerger@gmail.com>
2022-06-09 11:52:02 +0200

    added the comparison betweeen unmodified classical shadows and the local energy

--- deterministic/experiments/constants.py
+++
@@ -1,5 +0,0 @@
-BEST_ENERGIES_DIR = 'best_energies'
-BEST_MODELS_DIR = 'best_models'
-PLOTS_DIR = 'plots_dir'
-
-CHEMICAL_ACCURACY = 1.6e-3
--- deterministic/logging.py
+++
@@ -1,9 +0,0 @@
-import logging
-
-from abc import ABC
-
-
-class Logging(ABC):
-    def __init__(self):
-        super(Logging, self).__init__()
-        self.logger = logging.getLogger(f'nnqs.{self.__class__.__name__}')
